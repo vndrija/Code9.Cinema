2e3b7def408e69ca0bf711d794b14ca2b4f31a6b
vndrija <matijasevicicon@gmail.com>
2024-05-11 11:23:39 +0200

    created query, handler, and controller for get all cinemas

---
+++ Code9.API/Controllers/CinemaController.cs
@@ -0,0 +1,33 @@
+﻿using Code9.Domain.Models;
+using Code9.Domain.Queries;
+using MediatR;
+using Microsoft.AspNetCore.Components.Forms;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Code9.API.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class CinemaController : ControllerBase
+    {
+        private readonly IMediator _mediator;
+
+        public CinemaController(IMediator mediator)
+        {
+            _mediator = mediator;
+        }
+
+
+        [HttpGet]
+        public async Task<ActionResult<List<Cinema>>> GetAllCinemas()
+        {
+            var query = new GetAllCinemaQuery();
+            var result = await _mediator.Send(query);
+            return Ok(result);
+
+        }
+
+    }
+
+
+}
---
+++ Code9.Domain/Queries/GetAllCinemaQuery.cs
@@ -0,0 +1,12 @@
+﻿using Code9.Domain.Models;
+using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Code9.Domain.Queries
+{
+    public record GetAllCinemaQuery : IRequest<List<Cinema>>;
+}
