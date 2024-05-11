b8bf449d6b547b9d92d9bd929d5f2920082615a9
vndrija <matijasevicicon@gmail.com>
2024-05-11 12:18:39 +0200

    added model, command, handler for add cinema post method

---
+++ Code9.Domain/Models/AddCinemaRequest.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Code9.API
+{
+    public class AddCinemaRequest
+    {
+        [Required]
+        public string Name { get; set; }
+        public string City { get; set; }
+
+        public string Street { get; set; }
+
+        public int NumberOfAuditioriums { get; set; }
+
+
+    }
+}
